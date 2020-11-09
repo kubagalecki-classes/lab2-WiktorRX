507e0a8ad0bc712a9badaa3cc9401777b1e1dafd
WiktorRX <wiktorm@live.com>
2020-11-09 17:20:48 +0000

    11091820

--- lab2.cpp
+++ lab2.cpp
@@ -14,9 +14,8 @@ public:
         }
     }
 
-    double* A;
-    int     getDlugosc() { return dlugosc; };
-    void    print()
+    int  getDlugosc() { return dlugosc; };
+    void print()
     {
         cout << "[ ";
         int i = 0;
@@ -34,31 +33,62 @@ public:
                 do {
                     dlugosc--;
                     A[dlugosc] = 0.;
-                  } while (dlugosc > x);
-            else if (x > dlugosc) {dlugosc = x;}
+                } while (dlugosc > x);
+            else if (x > dlugosc) {
+                dlugosc = x;
+            }
         }
-        else
-        {
-          Wektor()
-        }
+        else {
+            cout << "dupa\n";
+            cout << "\n stworzono temp";
+            for (int i = 0; i < dlugosc; i++) {
+                temp[i] = A[i];
+            }
 
-        
+            delete[] A;
+
+            cout << "\n usunieto stare A fff";
+            cout << "\n stary A";
+
+            // A = new double[x];
+
+            cout << "\n nwy A 1";
+            cout << "\n nwy A 2";
+            for (int i = 0; i < dlugosc; i++) {
+                A[i] = temp[i];
+                cout << "bla";
+            }
+            for (int i = dlugosc; i < x; dlugosc++) {
+                A[i] = 0.;
+            }
+            delete[] temp;
+            dlugosc   = x;
+            pojemnosc = x;
+        }
     }
+    double* A;
 
 private:
-    int dlugosc;
-    int pojemnosc;
+    int     dlugosc;
+    int     pojemnosc;
+    double* temp;
 };
-
 int main()
 {
     Wektor Jessica{4};
-    Jessica.A[2] = 5;
+    Jessica.A[2] = 6969;
     cout << Jessica.A[2] << Jessica.A[3] << endl;
     cout << Jessica.getDlugosc() << endl;
     Jessica.print();
     Jessica.zmienDlugosc(3);
     Jessica.print();
-    Jessica.zmienDlugosc(4);
+    Jessica.zmienDlugosc(5);
+
+    for (int i = 0; i < Jessica.getDlugosc(); i++) {
+        cout << "bla";
+        Jessica.A[i] = i;
+    }
+    Jessica.print();
+    Jessica.zmienDlugosc(3);
     Jessica.print();
 }
