dd352bdbf5b781a769d2ef30e4faaefbc316a1fc
WiktorRX <73623657+WiktorRX@users.noreply.github.com>
2020-11-12 00:00:08 +0100

    Update ResourceManager.hpp

--- zal/ResourceManager.hpp
+++ zal/ResourceManager.hpp
@@ -5,34 +5,33 @@
 class ResourceManager
 {
   public:
-  ResourceManager() /* na stercie, wskaźnik do r lista inic */
+  ResourceManager()
   {r = new Resource;}
 
-  ~ResourceManager() {delete r}
+  ~ResourceManager() {delete r;}
 
-  ResourceManager(const ResourceManager& rm) /* stwórz nowy obiekt będący kopią *(rm.r) i przepisze jego adres do r    */
-  {r = new Resource{*rm.r}}
+  ResourceManager(const ResourceManager& rm)
+  {r = new Resource{*rm.r};}
 
 
   ResourceManager operator=(const ResourceManager& rm)
   {
-    //zwolnij bieżący zasób na ktory wskazuje return - nie usuniemy to wyciek pamięci
-    //swtórz nowy obiekt będący kopia *(rm.r) i przypisz jego adres do return
+  
            if (&rm != this) {
             delete r;
             r = new Resource{*rm.r}; 
         }
     return *this;
   }
-
+double get() {return r->get();}
   ResourceManager(ResourceManager&& rm)
   {
-    //sprawdz czy &rm i this to nie to samo, jeśli tak, nie rób nic
+
     r = nullptr;
     r=rm.r;
     rm.r=nullptr;
   }
-
+private:
   Resource *r;
-    double get() {return r->get();}
+  
 };
