5c588c2db9996ce28a75ee9a932f806fb16ab425
WiktorRX <73623657+WiktorRX@users.noreply.github.com>
2020-11-13 14:24:21 +0100

    13111424

--- zal/ResourceManager.hpp
+++ zal/ResourceManager.hpp
@@ -4,36 +4,53 @@
 
 class ResourceManager
 {
-  public:
-  ResourceManager()
-  {r = new Resource;}
+  Resource* r;
 
-  ~ResourceManager() {delete r;}
+public:
 
-  ResourceManager(const ResourceManager& rm)
-  {r = new Resource{*rm.r};}
+    ResourceManager()
+    {
+      r = new Resource;
+    }
 
+    ResourceManager(const ResourceManager& rm)
+    {
+      r = new Resource{*rm.r};
+    }
 
-  ResourceManager operator=(const ResourceManager& rm)
-  {
-  
-           if (&rm != this) {
+    ResourceManager(ResourceManager&& rm)
+    { 
+      r = nullptr;
+      r = rm.r;
+      rm.r = nullptr;
+    }
+
+    ~ResourceManager() 
+    {
+      delete r;
+    }
+
+    double get() 
+    { 
+      return r->get(); 
+    }
+
+    ResourceManager operator=(const ResourceManager& rm)
+    {
+        if (&rm != this) {
             delete r;
             r = new Resource{*rm.r}; 
         }
-    return *this;
-  }
+        return *this;
+    }
 
- 
-double get() {return r->get();}
-  ResourceManager(ResourceManager&& rm)
-  {
-
-    r = nullptr;
-    r=rm.r;
-    rm.r=nullptr;
-  }
-private:
-  Resource *r;
-  
+    ResourceManager operator=(ResourceManager&& rm)
+    {
+        if (&rm != this) {
+            delete r;
+            r = rm.r;
+            rm.r = nullptr;
+        }
+        return *this;
+    }
 };
